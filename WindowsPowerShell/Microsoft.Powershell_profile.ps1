9de0b6104fe7f0021bd93b8b94c8b7856d92d910
dbusteed <busteed.davis@gmail.com>
2019-12-12 15:20:50 -0700

    updates, cleaning

--- WindowsPowerShell/Microsoft.Powershell_profile.ps1
+++ WindowsPowerShell/Microsoft.Powershell_profile.ps1
@@ -1,18 +1,24 @@
 cd $home
 clear
 
-$block = @"
-    ____              _         ____             __                __
-   / __ \____ __   __(_)____   / __ )__  _______/ /____  ___  ____/ /
-  / / / / __  / | / / / ___/  / __  / / / / ___/ __/ _ \/ _ \/ __  / 
- / /_/ / /_/ /| |/ / (__  )  / /_/ / /_/ (__  ) /_/  __/  __/ /_/ /  
-/_____/\__,_/ |___/_/____/  /_____/\__,_/____/\__/\___/\___/\__,_/                                                   
-"@
-
-echo $block
-echo ""
-
 function Prompt { 
 	$ESC = [char]27
-	"`n$ESC[40;96m[ $((get-item $pwd).Name) ]: $ESC[0m" 
+    
+    $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
+
+    $prompt = "$ESC[40;97m[ $ESC[40;95m$(Get-Date -Format `"HH:mm:ss`") $ESC[40;97m/ $ESC[40;96m$((get-item $pwd).Name) $ESC[40;97m] $ESC[0m"
+
+	If($env:VIRTUAL_ENV) {
+        $venv = Split-Path $env:VIRTUAL_ENV -Leaf
+
+        $prompt = "$ESC[40;92m($venv) " + $prompt
+    }
+    
+    # add new line before prompt
+    $prompt = "`n" + $prompt
+    
+    $prompt
 }
+	
+# $ESC[BG;FGm
+# https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
